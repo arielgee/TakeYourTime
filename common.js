740181b2023d40dcbcf22268b4a44e7784850983
arielgee <ariel.hazard@gmail.com>
2018-12-22 21:07:13 +0200

    calculator module and more

--- common.js
+++ common.js
@@ -3,15 +3,193 @@
 /***************************************/
 let globals = (function () {
 
+	let manifest = browser.runtime.getManifest();
+
 	return {
+		MSG_PREF_HIGHLIGHT_PREFERENCE: "_msgPref_highlightPreference_",
+		MSG_BKGD_REFRESH_BROWSER_ACTION_ICON: "_msgBkgd_refreshBrowserActionIcon_",
+		MSG_BKGD_START_REFRESH_INTERVAL: "_msgBkgd_startRefreshInterval_",
+
+		HTMLID_ELEMENT_DAY: "pBarDay",
+		HTMLID_ELEMENT_MONTH: "pBarMonth",
+		HTMLID_ELEMENT_YEAR: "pBarYear",
+		HTMLID_ELEMENT_LIFE: "pBarLife",
+		HTMLID_ELEMENT_USER: "pBarUser",
+
+		WEB_EXT_NAME: manifest.name,
+		WEB_EXT_VERSION: manifest.version,
 		GEO_LOCATION_NOT_SET: "_geoLocationNotSet_",
 		URL_WHO_LIFE_EXPECTANCY_DATA: "/data/WHO2016data.json",
 		ICONIZED_PROGRESS_BAR_ID_NOT_SET: "_iconizedProgressBarIdNotSet_",
+		MILLISEC_IN_MINUTE: 60000,
 		MILLISEC_IN_DAY: 86400000,
 		MILLISEC_IN_YEAR: 31536000000,       // ignoring leap years
 	}
 })();
 
+/***************************************/
+let calculator = (function () {
+
+	////////////////////////////////////////////////////////////////////////////////////
+	function getElapsedPercentageForElementId(id, now) {
+
+		return new Promise((resolve) => {
+
+			let calcFunc;
+
+			switch (id) {
+				case globals.HTMLID_ELEMENT_DAY:	calcFunc = getDayElapsedPercentage;		break;
+				case globals.HTMLID_ELEMENT_MONTH:	calcFunc = getMonthElapsedPercentage;	break;
+				case globals.HTMLID_ELEMENT_YEAR:	calcFunc = getYearElapsedPercentage;	break;
+				case globals.HTMLID_ELEMENT_LIFE:	calcFunc = getLifeElapsedPercentage;	break;
+				case globals.HTMLID_ELEMENT_USER:	calcFunc = getUserElapsedPercentage;	break;
+			}
+
+			calcFunc(now).then((result) => resolve(result));
+		});
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////
+	function getDayElapsedPercentage(now) {
+
+		return new Promise((resolve) => {
+
+			prefs.getDayStart().then((start) => {
+				prefs.getDayEnd().then((end) => {
+					let total = (end - start) * 60;                                  // minutes in day
+					let elapsed = (now.getHours() - start) * 60 + now.getMinutes();  // minutes elapsed
+					let percent = Math.min(Math.max(Math.round(elapsed * 100 / total), 0), 100);
+
+					resolve({ percent: percent, title: "Your Day" });
+				})
+			});
+		});
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////
+	function getMonthElapsedPercentage(now) {
+
+		return new Promise((resolve) => {
+
+			let total = (new Date(now.getFullYear(), now.getMonth(), 0)).getDate() * 24;    // hours in this month
+			let elapsed = ((now.getDate() - 1) * 24) + now.getHours();                      // hours elapsed
+			let percent = Math.min(Math.max(Math.round(elapsed * 100 / total), 0), 100);
+
+			resolve({ percent: percent, title: "Your Month" });
+		});
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////
+	function getYearElapsedPercentage(now) {
+
+		return new Promise((resolve) => {
+
+			let total = (isLeapYear(now.getFullYear()) ? 366 : 365);                                            // days in this year
+			let elapsed = Math.ceil((now - (new Date(now.getFullYear(), 0, 1))) / globals.MILLISEC_IN_DAY);     // days elapsed
+			let percent = Math.min(Math.max(Math.round(elapsed * 100 / total), 0), 100);
+
+			resolve({ percent: percent, title: "Your Year" });
+		});
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////
+	function getLifeElapsedPercentage(now) {
+
+		return new Promise((resolve) => {
+
+			prefs.getGeoLocation().then((geoLocation) => {
+				prefs.getDateOfBirth().then((dateOfBirth) => {
+
+					if (geoLocation !== globals.GEO_LOCATION_NOT_SET && utils.isValidBirthDate(dateOfBirth)) {
+
+						prefs.getGender().then((gender) => {
+							utils.getJsonTextData(globals.URL_WHO_LIFE_EXPECTANCY_DATA).then((jsonText) => {
+
+								let whoData = JSON.parse(jsonText);
+								let nodes = getLifeExpectancyNode(geoLocation, whoData);
+								let years = Object.getOwnPropertyDescriptor(nodes[0], gender);
+
+								let total = years.value;                                                            // expectancy years
+								let elapsed = Math.floor((now - new Date(dateOfBirth)) / globals.MILLISEC_IN_YEAR); // years elapsed - ignoring leap years
+								let percent = Math.min(Math.max(Math.round(elapsed * 100 / total), 0), 100);
+
+								resolve({ percent: percent, title: "Your Life" });
+							});
+						});
+
+					} else {
+						resolve({ percent: null });
+					}
+				});
+			});
+		});
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////
+	function getUserElapsedPercentage(now) {
+
+		return new Promise((resolve) => {
+
+			prefs.getUserTitle().then((title) => {
+				prefs.getUserStartDate().then((startDate) => {
+					prefs.getUserEndDate().then((endDate) => {
+
+						let percent;
+
+						if (title !== "" && utils.isValidDate(startDate) && utils.isValidDate(endDate) && endDate > startDate) {
+
+							let total = ((new Date(endDate)) - (new Date(startDate))) / globals.MILLISEC_IN_DAY;   // days in range
+							let elapsed = (now - new Date(startDate)) / globals.MILLISEC_IN_DAY;                   // days elapsed
+							percent = Math.min(Math.max(Math.round(elapsed * 100 / total), 0), 100);
+						} else {
+							percent = null;
+						}
+						resolve({ percent: percent, title: title });
+					});
+				});
+			});
+		});
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////
+	function isLeapYear(year) {
+		return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////
+	function getLifeExpectancyNode(name, whoData) {
+
+		let node, nodes = [];
+
+		for (let n in whoData) {
+
+			if (!whoData.hasOwnProperty(n)) {
+				continue;
+			}
+
+			node = whoData[n];
+
+			if (typeof node === 'object') {
+				if (node.hasOwnProperty("name") && node.name === name) {
+					nodes.push(node);
+				} else {
+					nodes = nodes.concat(getLifeExpectancyNode(name, node));
+				}
+			}
+		}
+		return nodes;
+	}
+
+	return {
+		getElapsedPercentageForElementId: getElapsedPercentageForElementId,
+		getDayElapsedPercentage: getDayElapsedPercentage,
+		getMonthElapsedPercentage: getMonthElapsedPercentage,
+		getYearElapsedPercentage: getYearElapsedPercentage,
+		getLifeElapsedPercentage: getLifeElapsedPercentage,
+		getUserElapsedPercentage: getUserElapsedPercentage,
+	}
+
+})();
 
 /***************************************/
 let utils = (function () {
@@ -82,7 +260,57 @@ let utils = (function () {
 		} else {
 			elm.classList.remove("disabled");
 		}
-	};
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////
+	function getPercentImageData(percent) {
+
+		const id = "tytCanvasElapsedPercentage";
+		const bg = "#000000";
+		const fg = "#FFCF75";
+
+
+		let cnvs = document.getElementById(id);
+
+		if(cnvs === null) {
+			cnvs = document.createElement("canvas");
+			cnvs.id = id;
+			cnvs.width = cnvs.height = 16;
+		}
+
+		const ctx = cnvs.getContext("2d");
+
+		// background
+		ctx.fillStyle = bg;
+		ctx.fillRect(0, 0, cnvs.width, cnvs.height);
+
+		if (/^\d{1,3}%$/.test(percent)) {
+
+			// percent text
+			ctx.fillStyle = fg;
+			ctx.font = "10pt " + (percent === "100%" ? "Segoe UI" : "serif");
+			ctx.fillText(percent, 0.5, 13, cnvs.width - 0.5);
+		}
+
+		return ctx.getImageData(0, 0, cnvs.width, cnvs.height);
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////
+	function setBrowserActionDetails(percent, title) {
+
+		let iconDetails, titleDetails;
+
+		if(percent === null) {
+			iconDetails = {};
+			titleDetails = { title: null };
+		} else {
+			iconDetails = { imageData: utils.getPercentImageData(percent + "%") };
+			titleDetails = { title: globals.WEB_EXT_NAME + " - " + title };
+		}
+
+		browser.browserAction.setIcon(iconDetails);
+		browser.browserAction.setTitle(titleDetails);
+	}
 
 	return {
 		getJsonTextData: getJsonTextData,
@@ -90,5 +318,8 @@ let utils = (function () {
 		isValidBirthDate: isValidBirthDate,
 		blinkElement: blinkElement,
 		disableElementTree: disableElementTree,
+		getPercentImageData: getPercentImageData,
+		setBrowserActionDetails: setBrowserActionDetails,
 	}
+
 })();
--- popup/popup.js
+++ popup/popup.js
@@ -30,13 +30,9 @@ let preferences = (function () {
 		m_elmBtnRefresh.addEventListener("click", onClickRefresh, true);
 		m_elmBtnPreferences.addEventListener("click", onClickPreferences, true);
 
-		m_elmVersionLabel.textContent = "v" + browser.runtime.getManifest().version;
-
 		refreshProgressBars();
 
-		prefs.getIconizedProgressBarId().then((idElm) => {
-			setTimeout(() => iconizeProgressBar(document.getElementById(idElm)), 200);
-		});
+		m_elmVersionLabel.textContent = "v" + globals.WEB_EXT_VERSION;
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////
@@ -65,145 +61,49 @@ let preferences = (function () {
 		let now = new Date();
 
 		// +++ Day
-		calcDayProgress(now).then((result) => {
-			setProgressBar(document.getElementById("pBarDay"), result.percentage);
+		calculator.getDayElapsedPercentage(now).then((result) => {
+			setProgressBar(document.getElementById(globals.HTMLID_ELEMENT_DAY), result.percent);
 		});
 
 		// +++ Month
-		calcMonthProgress(now).then((result) => {
-			setProgressBar(document.getElementById("pBarMonth"), result.percentage);
+		calculator.getMonthElapsedPercentage(now).then((result) => {
+			setProgressBar(document.getElementById(globals.HTMLID_ELEMENT_MONTH), result.percent);
 		});
 
 		// +++ Year
-		calcYearProgress(now).then((result) => {
-			setProgressBar(document.getElementById("pBarYear"), result.percentage);
+		calculator.getYearElapsedPercentage(now).then((result) => {
+			setProgressBar(document.getElementById(globals.HTMLID_ELEMENT_YEAR), result.percent);
 		});
 
 		// +++ Life
-		calcLifeProgress(now).then((result) => {
-			setProgressBar(document.getElementById("pBarLife"), result.percentage);
+		calculator.getLifeElapsedPercentage(now).then((result) => {
+			setProgressBar(document.getElementById(globals.HTMLID_ELEMENT_LIFE), result.percent);
 		});
 
 		// +++ User
-		calcUserProgress(now).then((result) => {
-			setProgressBar(document.getElementById("pBarUser"), result.percentage, result.title);
-		}).catch(() => { /* rejected due to no user progress displayed */ });
-	}
-
-	////////////////////////////////////////////////////////////////////////////////////
-	function calcDayProgress(now) {
-
-		return new Promise((resolve) => {
-
-			prefs.getDayStart().then((start) => {
-				prefs.getDayEnd().then((end) => {
-					let total = (end - start) * 60;                                  // minutes in day
-					let elapsed = (now.getHours() - start) * 60 + now.getMinutes();  // minutes elapsed
-					let percentage = Math.min(Math.max(Math.round(elapsed * 100 / total), 0), 100);
-
-					resolve({ percentage: percentage });
-				})
-			});
-		});
-	}
-
-	////////////////////////////////////////////////////////////////////////////////////
-	function calcMonthProgress(now) {
-
-		return new Promise((resolve) => {
-
-			let total = (new Date(now.getFullYear(), now.getMonth(), 0)).getDate() * 24;    // hours in this month
-			let elapsed = ((now.getDate() - 1) * 24) + now.getHours();                      // hours elapsed
-			let percentage = Math.min(Math.max(Math.round(elapsed * 100 / total), 0), 100);
-
-			resolve({ percentage: percentage });
-		});
-	}
-
-	////////////////////////////////////////////////////////////////////////////////////
-	function calcYearProgress(now) {
-
-		return new Promise((resolve) => {
-
-			let total = (isLeapYear(now.getFullYear()) ? 366 : 365);                                            // days in this year
-			let elapsed = Math.ceil((now - (new Date(now.getFullYear(), 0, 1))) / globals.MILLISEC_IN_DAY);     // days elapsed
-			let percentage = Math.min(Math.max(Math.round(elapsed * 100 / total), 0), 100);
-
-			resolve({ percentage: percentage });
-		});
-	}
-
-	////////////////////////////////////////////////////////////////////////////////////
-	function calcLifeProgress(now) {
-
-		return new Promise((resolve) => {
-
-			prefs.getGeoLocation().then((geoLocation) => {
-				prefs.getDateOfBirth().then((dateOfBirth) => {
-
-					if (geoLocation !== globals.GEO_LOCATION_NOT_SET && utils.isValidBirthDate(dateOfBirth)) {
-
-						prefs.getGender().then((gender) => {
-							utils.getJsonTextData(globals.URL_WHO_LIFE_EXPECTANCY_DATA).then((jsonText) => {
-
-								let whoData = JSON.parse(jsonText);
-								let nodes = getLifeExpectancyNode(geoLocation, whoData);
-								let years = Object.getOwnPropertyDescriptor(nodes[0], gender);
-
-								let total = years.value;                                                            // expectancy years
-								let elapsed = Math.floor((now - new Date(dateOfBirth)) / globals.MILLISEC_IN_YEAR); // years elapsed - ignoring leap years
-								let percentage = Math.min(Math.max(Math.round(elapsed * 100 / total), 0), 100);
-
-								resolve({ percentage: percentage });
-							});
-						});
-
-					} else {
-						resolve({ percentage: null });
-					}
+		prefs.getUserProgressBar().then((checked) => {
+			document.getElementById("userProgressBarContainer").style.display = (checked ? "block" : "none");
+			if(checked) {
+				calculator.getUserElapsedPercentage(now).then((result) => {
+					setProgressBar(document.getElementById(globals.HTMLID_ELEMENT_USER), result.percent, result.title);
 				});
-			});
+			}
+		});
+
+		// ++ set iconized check mark
+		prefs.getIconizedProgressBarId().then((id) => {
+
+			if(id !== globals.ICONIZED_PROGRESS_BAR_ID_NOT_SET) {
+
+				setProgressBarIconizeCheckMark(id);
+				// Make sure both displays are synchronized (popup & iconized)
+				browser.runtime.sendMessage({ msgId: globals.MSG_BKGD_REFRESH_BROWSER_ACTION_ICON });
+			}
 		});
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////
-	function calcUserProgress(now) {
-
-		return new Promise((resolve, reject) => {
-
-			prefs.getUserProgressBar().then((checked) => {
-
-				document.getElementById("userProgressBarContainer").style.display = (checked ? "block" : "none");
-
-				if (checked) {
-
-					prefs.getUserTitle().then((title) => {
-						prefs.getUserStartDate().then((startDate) => {
-							prefs.getUserEndDate().then((endDate) => {
-
-								let percentage;
-
-								if (title !== "" && utils.isValidDate(startDate) && utils.isValidDate(endDate) && endDate > startDate) {
-
-									let total = ((new Date(endDate)) - (new Date(startDate))) / globals.MILLISEC_IN_DAY;   // days in range
-									let elapsed = (now - new Date(startDate)) / globals.MILLISEC_IN_DAY;                   // days elapsed
-									percentage = Math.min(Math.max(Math.round(elapsed * 100 / total), 0), 100);
-								} else {
-									percentage = null;
-								}
-								resolve({ percentage: percentage, title: title });
-							});
-						});
-					});
-				} else {
-					reject();
-				}
-			});
-		});
-	}
-
-	////////////////////////////////////////////////////////////////////////////////////
-	function setProgressBar(elmProgressBar, percentage, title = undefined) {
+	function setProgressBar(elmProgressBar, percent, title = undefined) {
 
 		if (elmProgressBar) {
 
@@ -217,7 +117,7 @@ let preferences = (function () {
 			let elmPrgBar = elmProgressBar.querySelector(".progressBar");
 			let elmInner = elmProgressBar.querySelector(".progressBar > .progressBarInner");
 
-			if (percentage === undefined || percentage === null) {
+			if (percent === undefined || percent === null) {
 
 				if (elmPref) elmPref.style.visibility = "visible";
 				elmValue.textContent = "\u2013";    // EN DASH (large hyphen)
@@ -227,39 +127,10 @@ let preferences = (function () {
 				setTimeout(() => utils.blinkElement(elmPref, 200, 2000), 750);
 			} else {
 				if (elmPref) elmPref.style.visibility = "";
-				elmValue.textContent = elmInner.style.width = percentage + "%";
+				elmValue.textContent = elmInner.style.width = percent + "%";
 				elmPrgBar.classList.remove("optionsNotSet");
 			}
 		}
-	}
-
-	////////////////////////////////////////////////////////////////////////////////////
-	function isLeapYear(year) {
-		return year % 400 === 0 || (year % 100 !== 0 && year % 4 === 0);
-	}
-
-	////////////////////////////////////////////////////////////////////////////////////
-	function getLifeExpectancyNode(name, whoData) {
-
-		let node, nodes = [];
-
-		for (let n in whoData) {
-
-			if (!whoData.hasOwnProperty(n)) {
-				continue;
-			}
-
-			node = whoData[n];
-
-			if (typeof node === 'object') {
-				if (node.hasOwnProperty("name") && node.name === name) {
-					nodes.push(node);
-				} else {
-					nodes = nodes.concat(getLifeExpectancyNode(name, node));
-				}
-			}
-		}
-		return nodes;
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////
@@ -281,12 +152,16 @@ let preferences = (function () {
 			elmPBarContainer = elmPBarContainer.parentElement;
 		}
 
-		prefs.getIconizedProgressBarId().then((idElm) => {
+		prefs.getIconizedProgressBarId().then((id) => {
 
-			if (idElm !== globals.ICONIZED_PROGRESS_BAR_ID_NOT_SET && idElm !== elmPBarContainer.id) {
-				document.getElementById(idElm).classList.remove("iconized");
+			if(id === elmPBarContainer.id) {
+				prefs.setIconizedProgressBarId(globals.ICONIZED_PROGRESS_BAR_ID_NOT_SET);
+			} else {
+				prefs.setIconizedProgressBarId(elmPBarContainer.id);
 			}
-			iconizeProgressBar(elmPBarContainer);
+			setProgressBarIconizeCheckMark(elmPBarContainer.id);
+
+			browser.runtime.sendMessage({ msgId: globals.MSG_BKGD_START_REFRESH_INTERVAL });
 		});
 	}
 
@@ -298,7 +173,10 @@ let preferences = (function () {
 		browser.runtime.openOptionsPage().then(() => {
 
 			setTimeout(() => {
-				browser.runtime.sendMessage({ progressBarId: event.target.parentElement.parentElement.id });
+				browser.runtime.sendMessage({
+					msgId: globals.MSG_PREF_HIGHLIGHT_PREFERENCE,
+					progressBarId: event.target.parentElement.parentElement.id
+				});
 				window.close();
 			}, 100);
 		});
@@ -322,56 +200,15 @@ let preferences = (function () {
 	}
 
 	////////////////////////////////////////////////////////////////////////////////////
-	function iconizeProgressBar(elmPBarContainer) {
+	function setProgressBarIconizeCheckMark(id) {
 
-		if (!elmPBarContainer || elmPBarContainer === undefined) {
-			return;
-		}
-
-		let iconDetails = {};
-		let titleDetails = { title: null };
-
-		if (elmPBarContainer.classList.contains("iconized")) {
-
-			elmPBarContainer.classList.remove("iconized");
-			prefs.setIconizedProgressBarId(globals.ICONIZED_PROGRESS_BAR_ID_NOT_SET);
-
-		} else {
-
-			elmPBarContainer.classList.add("iconized");
-			prefs.setIconizedProgressBarId(elmPBarContainer.id);
-
-			iconDetails = { imageData: createPercentageImage(elmPBarContainer.querySelector(".progressBarValue").textContent) };
-			titleDetails = { title: "Take Your Time - " + elmPBarContainer.querySelector(".progressBarTitle").textContent };
-		}
-
-		browser.browserAction.setIcon(iconDetails);
-		browser.browserAction.setTitle(titleDetails);
-	}
-
-	////////////////////////////////////////////////////////////////////////////////////
-	function createPercentageImage(percentage) {
-
-		const bg = "#000000";
-		const fg = "#FFCF75";
-
-		const cnvs = document.getElementById("canvasBrowserAction");
-		cnvs.width = cnvs.height = 16;
-		const ctx = cnvs.getContext("2d");
-
-		// background
-		ctx.fillStyle = bg;
-		ctx.fillRect(0, 0, cnvs.width, cnvs.height);
-
-		if (/^\d{1,3}%$/.test(percentage)) {
-
-			// percentage text
-			ctx.fillStyle = fg;
-			ctx.font = "10pt " + (percentage === "100%" ? "Segoe UI" : "serif");
-			ctx.fillText(percentage, 0.5, 13, cnvs.width - 0.5);
-		}
-
-		return ctx.getImageData(0, 0, cnvs.width, cnvs.height);
+		document.querySelectorAll(".progressBarContainer").forEach((elm, key, parent) => {
+			if(elm.id !== id || elm.classList.contains("iconized")) {
+				elm.classList.remove("iconized");
+			} else {
+				elm.classList.add("iconized");
+			}
+		});
 	}
 
 })();
